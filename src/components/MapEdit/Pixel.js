0ec346d4d19eb1d80f47bd964802eae388681687
Anmol Singh <ASingh2320@outlook.com>
2022-11-24 04:20:34 -0500

    layering works

--- src/components/MapEdit/Pixel.js
+++ src/components/MapEdit/Pixel.js
@@ -8,12 +8,17 @@ import { Box } from "@mui/system";
 
 
 
-const Pixel = ({tileData, currentTile,tileHeight, tileWidth, selectedTile, layerOrder, row, column}) => {
-    console.log("ii", row, column)
+const Pixel = ({tileData, currentTile,tileHeight, tileWidth, selectedTile, layerOrder, row, column, updateDataMap}) => {
+    //console.log("ii", row, column)
    let a=10
    let b =10
    const [pixelColor, setPixelColor]= useState("Gray");
-   const [layers, editLayer] = useState([]);
+   //const [layers, editLayer] = useState(tileData.layers);
+   const layers = tileData.layers;
+   if(row == 0 && column == 0){
+    console.log("00", layers);
+   }
+   
    const canvas = React.createRef();
 
    let pixels=[]
@@ -24,30 +29,47 @@ const Pixel = ({tileData, currentTile,tileHeight, tileWidth, selectedTile, layer
 
    }
    const drawOnCanvas = () => {
+    console.log("At draw on canvas");
     let last_layer = layerOrder[layerOrder.length - 1];
     console.log("last layer", last_layer);
     if(selectedTile.gid != -1){
-        const new_arr = [... layers];
-        new_arr[last_layer.id] = selectedTile;
-        editLayer(new_arr);
+        let new_arr =  JSON.parse(JSON.stringify(layers))
+        console.log("before", new_arr);
+        let {gid, data} = selectedTile;
+        let index = new_arr.findIndex(x => x.layer_id === last_layer.id);
+        if(index == -1){
+            new_arr.push({layer_id: last_layer.id, gid: gid, data: data});
+        }
+        else{
+           new_arr[index].gid = gid;
+           new_arr[index].data = data;
+        }
+        console.log("after", new_arr);
+        updateDataMap(row, column, new_arr);
+        //editLayer(new_arr);
         //doDrawing();
     }
    }
 
    const doDrawing = () => {
+    if(!layers){
+        return;
+    }
     let ctx = canvas.current.getContext("2d"); 
     ctx.clearRect(0, 0, canvas.current.width, canvas.current.height);
     let count = 0;
     if(row == 0, column == 0){
-        console.log("Layer", layers);
-        console.log("Order", layerOrder);
+        //console.log("Layer", layers);
+        //console.log("Order", layerOrder);
     }
     
     for(let i = 0; i < layerOrder.length; i++){
-        if(layers[layerOrder[i].id]){
+        let image_data = layers.find(x => x.layer_id === layerOrder[i].id);
+        if(image_data){
+            console.log("there is data");
             count = count + 1;
             let img = new Image;
-            img.src = layers[layerOrder[i].id].data;
+            img.src = image_data.data;
             ctx.drawImage(img, 0, 0);
         }
         else{
@@ -56,7 +78,8 @@ const Pixel = ({tileData, currentTile,tileHeight, tileWidth, selectedTile, layer
     }
    }
    useEffect(() => {
-    doDrawing();
+        console.log("Use effect going");
+      doDrawing();
    });
 
 
--- src/components/MapEdit/Row.js
+++ src/components/MapEdit/Row.js
@@ -9,7 +9,7 @@ import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 
 
 
-const Row = ({ rowData, mapWidth, row, key, currentTile,tileHeight, tileWidth, selectedTile, layerOrder}) => {
+const Row = ({ rowData, mapWidth, row, key, currentTile,tileHeight, tileWidth, selectedTile, layerOrder, updateDataMap}) => {
    
    let pixels=[]
     for (let i=0; i<mapWidth; i++){
@@ -20,8 +20,8 @@ const Row = ({ rowData, mapWidth, row, key, currentTile,tileHeight, tileWidth, s
         <div className="row">{
             rowData.map((tile, index) =>{
                 //console.log("id", key, index);
-                return <Pixel key={key + ' ' + index}  tileData = {tile} currentTile={currentTile} row={row} column={index} 
-                tileHeight={tileHeight} tileWidth={tileWidth} selectedTile={selectedTile}
+                return <Pixel key={row + ' ' + index}  tileData = {tile} currentTile={currentTile} row={row} column={index} 
+                tileHeight={tileHeight} tileWidth={tileWidth} selectedTile={selectedTile} updateDataMap={updateDataMap}
                 layerOrder={layerOrder}></Pixel>
             })
         }</div>
